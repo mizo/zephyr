d171a760a62524702d0a5dfa07e2796b6d7a3aa2
Daisuke Mizobuchi <mizo@atmark-techno.com>
2020-05-08 16:18:50 +0900

    arch: arm: cortex-m: add version section

---
+++ arch/arm/core/aarch32/cortex_m/version.c
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2020 Atmark Techno, Inc. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+
+#include <zephyr/types.h>
+#include <toolchain.h>
+#include <version.h>
+
+struct version {
+	const u8_t *build_version;
+	const u8_t *kernel_version;
+};
+
+Z_GENERIC_SECTION(.version) struct version _version = {
+#ifdef BUILD_VERSION
+	.build_version = STRINGIFY(BUILD_VERSION),
+#endif
+	.kernel_version = KERNEL_VERSION_STRING,
+};
